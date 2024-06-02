5cc6ca72bdbf82d77d1a83cc4184d6489dfdbf50
Sergio Herrera <chechoguz@gmail.com>
2024-06-02 04:18:33 -0400

    agregué funcionalidades solicitadas y ajustes pequeños de formato

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-precio = 400000
+const precio = 400000;
+const precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio.toLocaleString();
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+const cantidadSpan = document.querySelector('.cantidad');
+const valorTotalSpan = document.querySelector('.valor-total');
+const botonSumar = document.querySelector('.card button:nth-of-type(1)');
+const botonRestar = document.querySelector('.card button:nth-of-type(2)');
+
+function actualizarTotal() {
+  const cantidad = parseInt(cantidadSpan.textContent);
+  const total = cantidad * precio;
+  valorTotalSpan.textContent = total.toLocaleString();
+}
+
+actualizarTotal();
+
+botonSumar.addEventListener('click', () => {
+  cantidadSpan.textContent = parseInt(cantidadSpan.textContent) + 1;
+  actualizarTotal();
+});
+
+
+
+botonRestar.addEventListener('click', () => {
+  const nuevaCantidad = parseInt(cantidadSpan.textContent) - 1;
+  if (nuevaCantidad >= 0) {
+    cantidadSpan.textContent = nuevaCantidad;
+    actualizarTotal();
+  }
+});
